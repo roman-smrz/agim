349ee04a0be4ef5ae8c6a9fdf423097e2c89501a
Roman Smrž <roman.smrz@seznam.cz>
2011-02-14 22:55:10 +0100

    net: support for network mask

--- net.c
+++ net.c
@@ -9,12 +9,30 @@
 #include <netdb.h>
 
 
-static void parse_param(const char *param, sa_family_t *family,
-		size_t *addr_len, unsigned char **addr)
+inline static int max(int x, int y) { return x > y ? x : y; }
+
+
+/*
+ * Parses parameter into protocol family, address length and arrays of bytes of
+ * address and mask.
+ */
+static void parse_param(char *param, sa_family_t *family,
+		size_t *addr_len, unsigned char **addr, unsigned char **mask)
 {
+	// if there is a mask, divdide the parameter to address and mask part
+	char *addr_str = param, *mask_str = NULL;
+	for (char *c = param; *c; c++)
+		if (*c == '/') {
+			*c = '\0';
+			mask_str = c+1;
+			break;
+		}
+
+
+	// find address as first entry returned by getaddrinfo
 	struct addrinfo *addr_info;
 
-	int error = getaddrinfo(param, NULL, NULL, &addr_info);
+	int error = getaddrinfo(addr_str, NULL, NULL, &addr_info);
 	if (error != 0) {
 		fprintf(stderr, "net: getaddrinfo: %s\n", gai_strerror(error));
 		exit(EXIT_FAILURE);
@@ -24,7 +42,8 @@ static void parse_param(const char *param, sa_family_t *family,
 	if (*family == AF_INET) *addr_len = sizeof(struct in_addr);
 	if (*family == AF_INET6) *addr_len = sizeof(struct in6_addr);
 
-	*addr = malloc(*addr_len);
+	*addr = malloc((*addr_len)*2);
+	*mask = *addr + (*addr_len);
 
 	if (*family == AF_INET)
 		memcpy(*addr, &((struct sockaddr_in *) addr_info->ai_addr)->sin_addr, *addr_len);
@@ -32,6 +51,21 @@ static void parse_param(const char *param, sa_family_t *family,
 		memcpy(*addr, ((struct sockaddr_in6 *) addr_info->ai_addr)->sin6_addr.s6_addr, *addr_len);
 
 	freeaddrinfo(addr_info);
+
+	// if mask was given, set the bytes
+	if (mask_str) {
+		char *mask_after;
+		long mask_len = strtol(mask_str, &mask_after, 10);
+		if (*mask_after) {
+			fprintf(stderr, "net: \"%s\" is not valid mask size\n", mask_str);
+			exit(EXIT_FAILURE);
+		}
+		memset(*mask, 0, *addr_len);
+		for (unsigned int i = 0; i < *addr_len; i++, mask_len -= 8)
+			(*mask)[i] = 0xff << max(8 - mask_len, 0);
+	} else {
+		memset(*mask, 0xff, *addr_len);
+	}
 }
 
 
@@ -44,9 +78,9 @@ bool net(int argc, char **argv)
 
 	sa_family_t family;
 	size_t addr_len;
-	unsigned char *addr, *cur_addr;
+	unsigned char *addr, *cur_addr, *mask;
 
-	parse_param(argv[1], &family, &addr_len, &addr);
+	parse_param(argv[1], &family, &addr_len, &addr, &mask);
 
 
 	struct ifaddrs *ifaddr;
@@ -58,7 +92,7 @@ bool net(int argc, char **argv)
 
 	bool match_address() {
 		for (size_t i = 0; i < addr_len; i++) {
-			if (addr[i] != cur_addr[i])
+			if ((addr[i] & mask[i]) != (cur_addr[i] & mask[i]))
 				return false;
 		}
 		return true;
